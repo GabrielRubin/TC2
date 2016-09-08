3dfdbe1a2248d54e9c8b1d86f57bdd480a057815
BFortes <paz_bruno@hotmail.com>
2016-09-08 00:26:18 -0300

    added .pyi files (need more testing)

---
+++ Client/CatanPlayer.pyi
@@ -0,0 +1,25 @@
+from CatanGame import *
+
+class Player:
+
+    def __init__(self, name : str, seatNumber : int) -> None:
+        pass
+
+    def GetVictoryPoints(self) -> int:
+        pass
+
+    def DoMove(self, game : Game) -> object:
+        # type: (Game) -> object
+        pass
+
+    def ChooseCardsToDisard(self, game : Game):
+        pass
+
+class AgentRandom(Player):
+
+    def DoMove(self, game : Game) -> object:
+        # type: (Game) -> object
+        pass
+
+    def ChooseCardsToDiscard(self, game : Game):
+        pass
